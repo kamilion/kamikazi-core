392007e855b0c125f04816e4b96d734f5050987c
Graham Cantin <Kamilion@gmail.com>
2013-10-24 13:59:23 -0700

    fix directory

--- tools/post-update.sh
+++ tools/post-update.sh
@@ -2,7 +2,7 @@
 
 # Check to see if the disker-terminal icon exists. If not, make it.
 if [ ! -f /home/ubuntu/Desktop/disker-terminal.desktop ]; then
-    cp shortcuts/disker-terminal.desktop /home/ubuntu/Desktop/
+    cp /home/git/zurfa-deploy/shortcuts/disker-terminal.desktop /home/ubuntu/Desktop/
     chown 999:999 /home/ubuntu/Desktop/disker-terminal.desktop
     echo "post-update: Added disker-terminal icon."
 fi
