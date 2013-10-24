482ebcf5e565c7045e87613e6367c427aa01809a
Graham Cantin <Kamilion@gmail.com>
2013-10-24 13:50:19 -0700

    try to add an icon with a post-update action.

--- tools/post-update.sh
+++ tools/post-update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check to see if the disker-terminal icon exists. If not, make it.
+if [ ! -f /home/ubuntu/Desktop/disker-terminal.desktop ]; then
+    cp shortcuts/disker-terminal.desktop /home/ubuntu/Desktop/
+    chown 999:999 /home/ubuntu/Desktop/disker-terminal.desktop
+fi
+
 echo "post-update: Nothing to do."
 
 
