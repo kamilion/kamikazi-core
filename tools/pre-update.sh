2e1e29c902775d43bcb9c3cf1ef33e7cb93d5116
Graham Cantin <kamilion@gmail.com>
2013-10-25 04:14:45 -0700

    Refactored startup code in prep for image v0.5.0

--- tools/pre-update.sh
+++ tools/pre-update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "pre-update: Nothing to do."
+# We have no idea how many disks are connected. Hopefully it's less than 96.
+echo "pre-update: v0.5.0 Image: Turning off all swap."
+
+for i in {0..96}
+  do
+    swapoff -a
+  done
+
+echo "pre-update: Nothing left to do."
 
 
