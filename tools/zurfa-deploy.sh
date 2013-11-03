0277ac7a0db1c8c055b219e47b081aa7f131629d
Graham Cantin <kamilion@gmail.com>
2013-11-03 00:06:38 -0700

    Move boot-script over to zurfa-boot.sh

--- tools/zurfa-deploy.sh
+++ tools/zurfa-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# V0.5.0 uses this instead of post-boot.sh
+# V0.5.0 uses this from supervisor or serf triggered deploys.
 
 # Get into our main directory for it to be the CWD for the rest.
 cd /home/git/
