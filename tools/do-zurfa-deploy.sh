d14501924ba9d3299c6c3dd1ad9ddc0a44216754
Graham Cantin <kamilion@gmail.com>
2013-11-04 08:52:05 -0800

    Add another helper so serf doesn't get hung up spawning a redeploy that restarts serf.

---
+++ tools/do-zurfa-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# V0.5.0 Runs this script during a serf deploy to deal with platform updates.
+
+# NOTE: THE ISO FILENAME MUST STAY THE SAME!
+ZU_URL="http://10.0.5.253/files/tmp/kamikazi.iso"
+
+# Get into our main directory for it to be the CWD for the rest.
+cd /home/git/
+ZDHOME="/home/git/zurfa-deploy"
+ZDRES="${ZDHOME}/resources"
+
+echo "do-zurfa-deploy: Going to deploy in the background."
+
+${ZDHOME}/tools/zurfa-deploy.sh &
