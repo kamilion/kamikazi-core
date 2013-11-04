763a49b645cf781d56d2f553d3307a606be940b1
Graham Cantin <kamilion@gmail.com>
2013-11-04 08:34:33 -0800

    Allow the shutdown/reboot scripts to be targeted and take a time, but default to 1 minute. ${2:-1} means ${2} with the default of 1, even if the var is declared and null, which ${2} will be if it was passed no value.

--- tools/zurfa-shutdown.sh
+++ tools/zurfa-shutdown.sh
@@ -2,5 +2,13 @@
 
 # V0.5.0 uses this to shut down.
 
-shutdown -h 1
+MYNAME=$(hostname)
+echo -n "zurfa-shutdown: We are: ${MYNAME}"
+
+if [ "${1}" == "all" ]; then
+  shutdown -h ${2:-1}
+fi
+if [ "${1}" == "${MYNAME}" ]; then
+  shutdown -h ${2:-1}
+fi
 
