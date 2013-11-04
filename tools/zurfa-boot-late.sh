55dd95af66c3ebddf357ce47c7c3daeb4359d19e
Graham Cantin <kamilion@gmail.com>
2013-11-04 03:39:40 -0800

    Lots of little mistakes fixed.

--- tools/zurfa-boot-late.sh
+++ tools/zurfa-boot-late.sh
@@ -17,19 +17,23 @@ echo -n "zurfa-boot-late: We are: ${MYNAME}"
 
 if [ "${MYNAME}" == "wipemaster" ]; then
     echo "zurfa-boot-late-boot: I am the wipemaster, responsible for data storage."
-    echo -e "{\n  "role": "wipemaster"\n}\n" > ${ZDRES}/serf/config-role.json
+    echo -e '{\n  "role": "wipemaster"\n}\n' > ${ZDRES}/serf/config-role.json
     sync; sync; sync;
     echo "zurfa-boot-late-boot: Restarting serf agent to change role."
     service serf restart
+    echo "zurfa-boot-late-boot: Enabling swap for wipemaster."
+    swapon -a
     echo "zurfa-boot-late-boot: Copying ISO to ram to serve updates for outdated actors."
     cp /isodevice/boot/isos/kamikazi.iso /tmp/kamikazi.iso
-    # Mount some stuff and fiddle with rethink's storage location.
-    mount LABEL="wipemaster" /srv -o subvol=storage
-    swapon UUID="91cb9aed-d19d-4f94-8a1f-93c2e629ba71"
-    swapon UUID="011bd987-4492-4fd4-aa6b-e3c137db59e7"
-    # We shouldn't have a default instance running.
+    echo "zurfa-boot-late-boot: Enabling btrfs volume 'wipemaster'."
+    echo 'LABEL="wipemaster" /srv btrfs subvol=storage 0 0' >> /etc/fstab
+    mount /srv
+    echo "zurfa-boot-late-boot: Enabling rethinkdb instance 'wanwipemaster'."
     cp ${ZDRES}/rethink/wanwipemaster.conf /etc/rethinkdb/instances.d/wanwipemaster.conf
+    sync
+    sleep 1
     service rethinkdb start
+    echo "zurfa-boot-late-boot: Wipemaster should be active."
 else
     echo "zurfa-boot-late-boot: I am an actor, I must find a wipemaster."
     MASTER=$(serf members | grep wipemaster | sed "s/    /\t/g" | cut -f 2 )
@@ -38,10 +42,13 @@ else
     service rethinkdb stop
     sleep 3;  # Give rethink a moment to clean up
     echo "zurfa-boot-late-boot: I am an actor, pointing rethinkdb proxy at ${MASTER}."
-    # We shouldn't have a default instance running.
     cp ${ZDRES}/rethink/wanwipe.conf /etc/rethinkdb/instances.d/wanwipe.conf
+    sync
     echo -e "\njoin=${MASTER}:29015\n" >> /etc/rethinkdb/instances.d/wanwipe.conf
+    sync
+    sleep 1
     service rethinkdb start
+    echo "zurfa-boot-late-boot: Actor should be active."
 fi
 
 
