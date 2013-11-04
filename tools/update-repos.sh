aaa5628be6b90c82be30c02035b45a462f6a56ff
Graham Cantin <kamilion@gmail.com>
2013-11-04 02:04:37 -0800

    fix a future problem with wipemaster failing to update.

--- tools/update-repos.sh
+++ tools/update-repos.sh
@@ -3,5 +3,9 @@
 cd /home/git/
 # for each folder, git pull
 for D in */; do 
- cd ${D}; git reset --merge HEAD; git pull; cd ..;
+  cd ${D};  # Go into the repository.
+  git stash;  # Stash any changes.
+  git pull;  # Pull updates.
+  git stash pop;  # Unstash changes (May fail!)
+  cd ..;  # Go out of the repository.
 done
