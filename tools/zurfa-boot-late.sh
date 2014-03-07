5ba5fb117d0b79ed4adff6c4003710680d94ce77
Zurfa Deployment <support@zurfa.net>
2014-03-07 21:32:05 +0000

    Tinkered with late boot, see if this commits.

--- tools/zurfa-boot-late.sh
+++ tools/zurfa-boot-late.sh
@@ -29,14 +29,17 @@ if [ "${MYNAME}" == "wipemaster" ]; then
     echo 'LABEL="wipemaster" /srv btrfs subvol=storage 0 0' >> /etc/fstab
     btrfs device scan  # This should fix the ctree error.
     mount /srv
-    echo "zurfa-boot-late-boot: Enabling rethinkdb instance 'wanwipemaster'."
+    echo "zurfa-boot-late-boot: Enabling RethinkDB instance 'wanwipemaster'."
     cp ${ZDRES}/rethink/wanwipemaster.conf /etc/rethinkdb/instances.d/wanwipemaster.conf
     chown -R rethinkdb.rethinkdb /var/lib/rethinkdb
     sync
     sleep 1
     service rethinkdb start
-    echo "zurfa-boot-late-boot: RethinkDB is up, restarting diskmonitor."
-    supervisorctl restart zurfa-diskmonitor
+    echo "zurfa-boot-late-boot: RethinkDB instance 'wanwipemaster' is up"
+    echo "zurfa-boot-late-boot: Disabling diskmonitor for wipemaster."
+    supervisorctl stop zurfa-diskmonitor
+    echo "zurfa-boot-late-boot: Disabling diskworker for wipemaster."
+    supervisorctl stop zurfa-diskworker
     echo "zurfa-boot-late-boot: Wipemaster should be active."
 else
     echo "zurfa-boot-late-boot: I am an actor, I must find a wipemaster."
@@ -49,7 +52,7 @@ else
     echo "zurfa-boot-late-boot: I am an actor, I must stop rethinkdb."
     service rethinkdb stop
     sleep 3;  # Give rethink a moment to clean up
-    echo "zurfa-boot-late-boot: I am an actor, pointing rethinkdb proxy at ${MASTER}."
+    echo "zurfa-boot-late-boot: I am an actor, pointing RethinkDB proxy at ${MASTER}."
     cp ${ZDRES}/rethink/wanwipe.conf /etc/rethinkdb/instances.d/wanwipe.conf
     sync
     echo -e "\njoin=${MASTER}:29015\n" >> /etc/rethinkdb/instances.d/wanwipe.conf
