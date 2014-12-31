ac8de20603aace91bf65326c51207411f682e23d
Kamilion <kamilion@gmail.com>
2014-12-31 18:40:30 +0000

    Whoops, little cleaning

--- tools/vmtools.sh
+++ tools/vmtools.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ $(blkid -s LABEL -o value /dev/sr0) == "VMware Tools"]; then
+if [ "$(/sbin/blkid -s LABEL -o value /dev/sr0)" == "VMware Tools" ]; then
   mount /dev/sr0 /mnt
-  tar -C /tmp/ -xvf /mnt/VMwareTools-*.tar.gz
+  tar -C /tmp/ -xf /mnt/VMwareTools-*.tar.gz
   umount /dev/sr0
   /tmp/vmware-tools-distrib/vmware-install.pl -d
+  rm -Rf /tmp/vmware-tools-distrib/
   /usr/bin/vmware-user
 fi
 
