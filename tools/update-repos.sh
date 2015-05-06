c28b34f0c65979909836ed36130ac2b18264cadc
Kamilion <kamilion@gmail.com>
2015-05-06 04:42:57 -0700

    Another bugfix from @dwfreed to the stash-before-pull behavior.

--- tools/update-repos.sh
+++ tools/update-repos.sh
@@ -6,7 +6,7 @@ cd /home/git/
 # for each folder, git pull
 for D in */; do 
   cd ${D};  # Go into the repository.
-  git stash;  # Stash any changes.
+  git stash -u;  # Stash any changes.
   git pull;  # Pull updates.
   git stash pop;  # Unstash changes (May fail!)
   cd ..;  # Go out of the repository.
