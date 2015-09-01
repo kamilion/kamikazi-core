b48073e0ad106c4bccadc9c313d741388e2dfbfc
Graham Cantin <kamilion@gmail.com>
2015-09-01 07:52:29 -0700

    On second thought, move things around a little so we don't flub git.

--- resources/buildscripts/wily/autobuild.customizer.hook.sh
+++ resources/buildscripts/wily/autobuild.customizer.hook.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-echo "Installing git."
+echo "KAMIKAZI: Updating package lists..."
+apt update
+echo "KAMIKAZI: Installing git."
 apt-get install -y --no-install-recommends git
-echo "Configuring git."
+echo "KAMIKAZI: Configuring git."
 git config --global user.email "kamilion@gmail.com"
 git config --global user.name "Graham Cantin"
 git config --global push.default simple
 mkdir -p /home/git
 cd /home/git
-echo "Checking out kamikazi-deploy repository..."
+echo "KAMIKAZI: Checking out kamikazi-deploy repository..."
 git clone https://kamilion@github.com/kamilion/kamikazi-deploy.git
-echo "Attempting to rebuild ISO contents..."
+echo "KAMIKAZI: Attempting to rebuild ISO contents..."
 cd /home/git/kamikazi-deploy/resources/buildscripts/wily/
-apt update
 apt full-upgrade -y
 ./00-build-clean-iso-from-source.sh
+echo "KAMIKAZI: Autobuild complete."
+exit 0
+
