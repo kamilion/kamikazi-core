d36e6d5a2493c4f3c5d34baa57fc213e150f6d9a
Graham Cantin <kamilion@gmail.com>
2015-09-01 07:27:04 -0700

    Updated buildscripts & packages for wily as of 9-1-2015.

---
+++ resources/buildscripts/wily/00-build-clean-iso-from-source.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "[kamikazi-deploy] Building Clean ISO from lubuntu-15.04-amd64.iso"
+
+echo "[kamikazi-deploy] Replacing firefox with midori."
+packages=$(awk '{print $1} ' 01-add-replacement-browser.synpkg)
+apt-get install -y ${packages}
+echo "[kamikazi-deploy] Removing packages."
+packages=$(awk '{print $1} ' 02-purgelist.synpkg)
+apt-get purge -y ${packages}
+echo "[kamikazi-deploy] Adding base server packages."
+packages=$(awk '{print $1} ' 03-addlist.synpkg)
+echo ${packages}
+apt-get install -y ${packages}
+# Remove chronyd's pidfile.
+rm -f /run/chronyd.pid
+apt-get purge -y ntp
+echo "[kamikazi-deploy] Adding python 2.x and 3.x development kit."
+packages=$(awk '{print $1} ' 04-addlist-python-dev.synpkg)
+echo ${packages}
+apt-get install -y ${packages}
+echo "[kamikazi-deploy] Adding Xen hypervisor and openvswitch packages."
+packages=$(awk '{print $1} ' 05-addlist-xen.synpkg)
+echo ${packages}
+apt-get install -y ${packages}
+echo "[kamikazi-deploy] Adding Ceph network block device packages."
+packages=$(awk '{print $1} ' 06-addlist-ceph.synpkg)
+echo ${packages}
+apt-get install -y ${packages}
+
+# Remove this socket that causes unpacking squashfs to warn.
+rm -f /run/synaptic.socket
+
+echo "[kamikazi-deploy] Ready to Build ISO."
