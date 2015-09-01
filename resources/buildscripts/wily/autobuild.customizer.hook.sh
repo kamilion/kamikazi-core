8ea98c9a67f065a3c93b8c84189f754859cc648a
Graham Cantin <kamilion@gmail.com>
2015-09-01 07:36:56 -0700

    Added a hook script for customizer to run, which will check out the repository and prep a full ISO for build.

---
+++ resources/buildscripts/wily/autobuild.customizer.hook.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Installing git."
+apt-get install -y git
+echo "Configuring git."
+git config --global user.email "kamilion@gmail.com"
+git config --global user.name "Graham Cantin"
+git config --global push.default simple
+mkdir -p /home/git
+cd /home/git
+echo "Checking out kamikazi-deploy repository..."
+git clone https://kamilion@github.com/kamilion/kamikazi-deploy.git
+echo "Attempting to rebuild ISO contents..."
+cd /home/git/kamikazi-deploy/resources/buildscripts/wily/
+./00-build-clean-iso-from-source.sh
