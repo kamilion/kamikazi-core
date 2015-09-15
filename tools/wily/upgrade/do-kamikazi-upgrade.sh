681d6989a135493b8899d78cd14c02c53d964c0d
Graham Cantin <kamilion@gmail.com>
2015-09-15 13:19:35 -0700

    Bring the modified openvswitch initscript back, fix the upgrade autoreboot.

--- tools/wily/upgrade/do-kamikazi-upgrade.sh
+++ tools/wily/upgrade/do-kamikazi-upgrade.sh
@@ -3,7 +3,8 @@
 # V0.8.0 Runs this script during a deploy to deal with platform updates.
 
 # NOTE: THE ISO FILENAME MUST STAY THE SAME!
-KU_URL="http://10.0.5.253/files/tmp/kamikazi.iso"
+KU_HOST="10.0.5.253"
+KU_URL="http://${KU_HOST}/files/tmp/kamikazi.iso"
 
 # Get into our main directory for it to be the CWD for the rest.
 cd /home/git/
@@ -12,6 +13,15 @@ KCRES="${KCHOME}/resources/latest"
 
 echo "do-kamikazi-upgrade: Going to update from:"
 echo ${KU_URL}
+
+echo "do-kamikazi-upgrade: Pinging ${KU_HOST}:"
+if ! ping -c 3 ${KU_HOST}; then
+  echo "do-kamikazi-upgrade: Update server failed to respond, aborting upgrade."
+  exit 0;  # Exit this script early.
+else
+  echo "do-kamikazi-upgrade: Update server is responding, continuing upgrade."
+fi
+
 
 # Remove the old grub.cfg~ file.
 if [ -f /isodevice/boot/grub/grub.cfg~ ]; then
