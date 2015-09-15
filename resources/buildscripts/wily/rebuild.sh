e15bc831be7275f6b80cd60efe79097c7d7bbee7
Graham Cantin <kamilion@gmail.com>
2015-09-15 12:44:36 -0700

    Oops, should probably kill off wubi since it's so broken.

--- resources/buildscripts/wily/rebuild.sh
+++ resources/buildscripts/wily/rebuild.sh
@@ -7,6 +7,8 @@ echo "KAMIKAZI-REBUILD: Triggering custom hook..."
 customizer -k
 echo "KAMIKAZI-REBUILD: Fixing xen on ISO..."
 cp -f /home/ubuntu/git/kamikazi-core/resources/buildscripts/wily/isomods/* /home/kamikazi-15.10/ISO/isolinux/
+rm -f /home/kamikazi-15.10/ISO/wubi.exe
+rm -f /home/kamikazi-15.10/ISO/autorun.inf
 echo "KAMIKAZI-REBUILD: Building output ISO..."
 customizer -r
 echo "KAMIKAZI-REBUILD: Complete."
