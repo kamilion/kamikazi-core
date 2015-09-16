d3ebbe429e597d99cb253fa0c55e156ac3dd705a
Graham Cantin <kamilion@gmail.com>
2015-09-15 23:18:22 -0700

    fixing serf...

--- resources/buildscripts/wily/12-add-serf.sh
+++ resources/buildscripts/wily/12-add-serf.sh
@@ -8,9 +8,11 @@ cd /tmp/serfdom
 
 #if [ "$(uname -m)" == "x86_64" ]; then
 if [ -d /lib/x86_64-linux-gnu ]; then
-  wget https://dl.bintray.com/mitchellh/serf/0.6.4_linux_amd64.zip
+  wget http://files.sllabs.com/files/long-term/downloads/serf/0.6.4_linux_amd64.zip
+  # https://dl.bintray.com/mitchellh/serf/0.6.4_linux_amd64.zip
 elif [ -d /lib/i386-linux-gnu ]; then
-  wget https://dl.bintray.com/mitchellh/serf/0.6.4_linux_386.zip
+  wget http://files.sllabs.com/files/long-term/downloads/serf/0.6.4_linux_386.zip
+  # https://dl.bintray.com/mitchellh/serf/0.6.4_linux_386.zip
 fi
 
 unzip 0.6.4_linux_*.zip
