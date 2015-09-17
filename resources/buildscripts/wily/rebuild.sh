dae1975f6f73f39894756e45cf6bc6eede463ed9
Graham Cantin <kamilion@gmail.com>
2015-09-17 04:47:00 -0700

    Try to clean out the dist and pool folders on the ISO.

--- resources/buildscripts/wily/rebuild.sh
+++ resources/buildscripts/wily/rebuild.sh
@@ -7,8 +7,13 @@ echo "KAMIKAZI-REBUILD: Triggering custom hook..."
 customizer -k
 echo "KAMIKAZI-REBUILD: Fixing xen on ISO..."
 cp -f /home/ubuntu/git/kamikazi-core/resources/buildscripts/wily/isomods/* /home/kamikazi-15.10/ISO/isolinux/
+echo "KAMIKAZI-REBUILD: Removing WUBI from ISO..."
 rm -f /home/kamikazi-15.10/ISO/wubi.exe
 rm -f /home/kamikazi-15.10/ISO/autorun.inf
+rm -Rf /home/kamikazi-15.10/ISO/pics/
+echo "KAMIKAZI-REBUILD: Removing package pool from ISO..."
+rm -Rf /home/kamikazi-15.10/ISO/dist/
+rm -Rf /home/kamikazi-15.10/ISO/pool/
 echo "KAMIKAZI-REBUILD: Building output ISO..."
 customizer -r
 echo "KAMIKAZI-REBUILD: Complete."
