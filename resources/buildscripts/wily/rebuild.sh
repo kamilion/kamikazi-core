d38e807518c8d54b970e221e2ddc7c7e6d24a984
Graham Cantin <kamilion@gmail.com>
2015-09-21 08:36:56 -0700

    Removed the package caching, causing more problems than good.

--- resources/buildscripts/wily/rebuild.sh
+++ resources/buildscripts/wily/rebuild.sh
@@ -3,8 +3,6 @@ echo "KAMIKAZI-REBUILD: Cleaning up previous run..."
 customizer -t
 echo "KAMIKAZI-REBUILD: Extracting ISO..."
 customizer -e
-echo "KAMIKAZI-REBUILD: Restoring packages from previous run..."
-mv /home/kamikazi-15.10/archives /home/kamikazi-15.10/FileSystem/var/cache/apt/archives
 echo "KAMIKAZI-REBUILD: Triggering custom hook..."
 customizer -k
 echo "KAMIKAZI-REBUILD: Fixing xen on ISO..."
@@ -16,8 +14,11 @@ rm -Rf /home/kamikazi-15.10/ISO/pics/
 echo "KAMIKAZI-REBUILD: Removing package pool from ISO..."
 rm -Rf /home/kamikazi-15.10/ISO/dist/
 rm -Rf /home/kamikazi-15.10/ISO/pool/
-echo "KAMIKAZI-REBUILD: Saving packages from previous run..."
-mv /home/kamikazi-15.10/FileSystem/var/cache/apt/archives /home/kamikazi-15.10/archives
+echo "KAMIKAZI-REBUILD: Clearing annoyances from filesystem..."
+rm -Rf /home/kamikazi-15.10/FileSystem/var/cache/apt/archives
+mkdir -p /home/kamikazi-15.10/FileSystem/var/cache/apt/archives/partial
+echo "KAMIKAZI-REBUILD: Clearing apport crash reports from filesystem..."
+rm -f /home/kamikazi-15.10/FileSystem/var/crash/*
 echo "KAMIKAZI-REBUILD: Building output ISO..."
 customizer -r
 echo "KAMIKAZI-REBUILD: Complete."
