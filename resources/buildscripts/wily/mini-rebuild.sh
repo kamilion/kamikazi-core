cb2123f895041512f40903d5744a808f0c754ac0
Graham Cantin <kamilion@gmail.com>
2015-09-24 19:36:10 -0700

    Remove our git repo for minilubuntu.

--- resources/buildscripts/wily/mini-rebuild.sh
+++ resources/buildscripts/wily/mini-rebuild.sh
@@ -14,12 +14,13 @@ echo "KAMIKAZI-REBUILD: Removing package pool from ISO..."
 rm -Rf /home/minilubuntu/ISO/dists/
 rm -Rf /home/minilubuntu/ISO/pool/
 echo "KAMIKAZI-REBUILD: Clearing annoyances from filesystem..."
+rm -Rf /home/git
 rm -Rf /home/minilubuntu/FileSystem/var/cache/apt/archives
 mkdir -p /home/minilubuntu/FileSystem/var/cache/apt/archives/partial
 echo "KAMIKAZI-REBUILD: Clearing apport crash reports from filesystem..."
 rm -f /home/minilubuntu/FileSystem/var/crash/*
-echo "KAMIKAZI-REBUILD: Building output ISO..."
-customizer -r
+#echo "KAMIKAZI-REBUILD: Building output ISO..."
+#customizer -r
 echo "KAMIKAZI-REBUILD: Complete."
 exit 0
 
