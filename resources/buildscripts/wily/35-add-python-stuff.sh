2463c1e96b9f7ef6819b6d9905f0e4c294f1e9ec
Graham Cantin <kamilion@gmail.com>
2015-09-30 16:31:35 -0700

    Enable the uwsgi service.

--- resources/buildscripts/wily/35-add-python-stuff.sh
+++ resources/buildscripts/wily/35-add-python-stuff.sh
@@ -4,8 +4,16 @@ echo "[kamikazi-build] Adding more python stuff..."
 
 OLDDIR=${PWD}
 
+echo "[kamikazi-build] Compiling scrypt for python2..."
 pip2 install scrypt
-pip3 install uwsgi scrypt
+echo "[kamikazi-build] Compiling scrypt for python3..."
+pip3 install scrypt
+echo "[kamikazi-build] Compiling uwsgi for python3..."
+pip3 install uwsgi
+echo "[kamikazi-build] Removing uwsgi sysvinit script for python3..."
+rm /etc/init.d/uwsgi
+echo "[kamikazi-build] Enabling uwsgi emperor for python3..."
+systemctl enable emperor.uwsgi.service
 
 cd ${OLDDIR}
 
