d2354c3a4cd440360060618345872101518547e4
Graham Cantin <kamilion@gmail.com>
2015-12-29 10:42:57 -0800

    Update Rethinkdb to 2.2.2

--- resources/buildscripts/xenial/21-add-rethinkdb.sh
+++ resources/buildscripts/xenial/21-add-rethinkdb.sh
@@ -8,9 +8,9 @@ cd /tmp/rethinkdb
 
 #if [ "$(uname -m)" == "x86_64" ]; then
 if [ -d /lib/x86_64-linux-gnu ]; then
-  wget http://files.sllabs.com/files/long-term/downloads/packages/rethinkdb_2.2.1-0wily_amd64.deb
+  wget http://files.sllabs.com/files/long-term/downloads/packages/rethinkdb_2.2.2-0wily_amd64.deb
 elif [ -d /lib/i386-linux-gnu ]; then
-  wget http://files.sllabs.com/files/long-term/downloads/packages/rethinkdb_2.2.1-0wily_i386.deb
+  wget http://files.sllabs.com/files/long-term/downloads/packages/rethinkdb_2.2.2-0wily_i386.deb
 fi
 
 dpkg -i rethinkdb_2.*.deb
