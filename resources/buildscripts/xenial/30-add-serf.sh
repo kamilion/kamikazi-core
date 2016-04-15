f298c62610b07649d7f2a37d8aa3a5444ffbd390
Graham Cantin <kamilion@gmail.com>
2016-04-15 14:23:46 -0700

    Update to serf 0.7.0

--- resources/buildscripts/xenial/30-add-serf.sh
+++ resources/buildscripts/xenial/30-add-serf.sh
@@ -8,16 +8,18 @@ cd /tmp/serfdom
 
 #if [ "$(uname -m)" == "x86_64" ]; then
 if [ -d /lib/x86_64-linux-gnu ]; then
-  wget http://files.sllabs.com/files/long-term/downloads/serf/0.6.4_linux_amd64.zip
+  wget http://files.sllabs.com/files/long-term/downloads/serf/serf_0.7.0_linux_amd64.zip
+  # https://releases.hashicorp.com/serf/0.7.0/serf_0.7.0_linux_amd64.zip
   # https://dl.bintray.com/mitchellh/serf/0.6.4_linux_amd64.zip
 elif [ -d /lib/i386-linux-gnu ]; then
-  wget http://files.sllabs.com/files/long-term/downloads/serf/0.6.4_linux_386.zip
+  wget http://files.sllabs.com/files/long-term/downloads/serf/serf_0.7.0_linux_386.zip
+  # https://releases.hashicorp.com/serf/0.7.0/serf_0.7.0_linux_386.zip
   # https://dl.bintray.com/mitchellh/serf/0.6.4_linux_386.zip
 fi
 
-unzip 0.6.4_linux_*.zip
+unzip serf_0.7.0_linux_*.zip
 mv serf /usr/local/bin/serf
-rm 0.6.4_linux_*.zip
+rm serf_0.7.0_linux_*.zip
 cd /tmp
 rmdir serfdom
 
