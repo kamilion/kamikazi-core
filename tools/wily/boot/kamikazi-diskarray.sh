3b535eca95a02714f9b5eaad9924941c7d19f669
Graham Cantin <kamilion@gmail.com>
2016-08-03 23:39:04 -0700

    Unintended modules are getting loaded on systems that do not require them. Fix this by checking the PCI device IDs of common HBAs before loading drivers. Convert the module loaders into bash functions, call a function from an if tree of choices.

--- tools/wily/boot/kamikazi-diskarray.sh
+++ tools/wily/boot/kamikazi-diskarray.sh
@@ -2,36 +2,60 @@
 
 echo "kamikazi-diskarrays: Searching for disk array devices..."
 
+HBATYPE=""
+
+load_3w_9xxx() {
+    HBATYPE="3ware"
+    echo "kamikazi-diskarrays: Loading 3Ware 9XXX driver."
+    modprobe 3w_9xxx
+    return $?
+}
+
+load_lsisas3() {
+    HBATYPE="lsisas3"
+    echo "kamikazi-diskarrays: Loading LSI SAS3 driver."
+    modprobe mpt3sas
+    return $?
+}
+
+load_lsisas2() {
+    HBATYPE="lsisas2"
+    echo "kamikazi-diskarrays: Loading LSI SAS2 driver."
+    modprobe mpt2sas
+    return $?
+}
+
+load_lsisas1() {
+    HBATYPE="lsisas1"
+    echo "kamikazi-diskarrays: Loading LSI SAS1 driver."
+    modprobe mptsas
+    return $?
+}
+
+
 # Just try the drivers we blacklisted for now, in descending order.
-if modprobe 3w_9xxx ; then
-    echo "kamikazi-diskarrays: Loaded 3Ware 9XXX driver."
-else
-    echo "kamikazi-diskarrays: No 3Ware 9XXX found or driver failed to load."
-fi
+if [[ ! -z $(lspci -d 13c1:) ]]; then load_3w_9xxx(); fi
 
-# Try LSI SAS3xxx first. (SAS3008/3118)
-if modprobe mpt3sas ; then
-    echo "kamikazi-diskarrays: Loaded LSI SAS3xxx driver."
-else
-    echo "kamikazi-diskarrays: No LSI SAS3xxx found or driver failed to load."
-fi
+# Try LSI SAS3xxx first. (SAS3008/3118) -- Will also deal with 2XXX devices.
+if [[ ! -z $(lspci -d 1000:0097) ]]; then load_lsisas3(); fi  # LSI SAS 3008
+if [[ ! -z $(lspci -d 1000:0080) ]]; then load_lsisas3(); fi  # LSI SAS 2208
+if [[ ! -z $(lspci -d 1000:0073) ]]; then load_lsisas3(); fi  # LSI SAS 2008
+if [[ ! -z $(lspci -d 1000:0072) ]]; then load_lsisas3(); fi  # LSI SAS 2008
+if [[ ! -z $(lspci -d 1000:005b) ]]; then load_lsisas3(); fi  # LSI SAS 2118
+if [[ ! -z $(lspci -d 1000:) ]]; then load_lsisas3(); fi  # LSI SAS 3XXX/2XXX
 
-# Try LSI SAS2xxx second. (SAS2008/2118)
-if modprobe mpt2sas ; then
-    echo "kamikazi-diskarrays: Loaded LSI SAS2xxx driver."
-else
-    echo "kamikazi-diskarrays: No LSI SAS2xxx found or driver failed to load."
-fi
+# Try LSI SAS2xxx second. (SAS2008/2118) -- This is a noop if lsisas3 already got it.
+if [[ ! -z $(lspci -d 1000:) ]]; then load_lsisas2(); fi  # LSI SAS 2XXX (older)
 
 # Then try LSI SAS1xxx last. (SAS1068/1068E)
-if modprobe mptsas ; then
-    echo "kamikazi-diskarrays: Loaded LSI SAS1xxx driver."
-else
-    echo "kamikazi-diskarrays: No LSI SAS1xxx found or driver failed to load."
-fi
+if [[ ! -z $(lspci -d 1000:0050) ]]; then load_lsisas1(); fi  # LSI SAS1068E
+if [[ ! -z $(lspci -d 1000:0054) ]]; then load_lsisas1(); fi  # LSI SAS1068
+if [[ ! -z $(lspci -d 1000:0056) ]]; then load_lsisas1(); fi  # LSI SAS1064ET
+if [[ ! -z $(lspci -d 1000:0058) ]]; then load_lsisas1(); fi  # LSI SAS1068E
+if [[ ! -z $(lspci -d 1000:005e) ]]; then load_lsisas1(); fi  # LSI SAS1066
 
 # Okay, we should have succeeded in loading zero or more of the SAS drivers.
-echo "kamikazi-diskarrays: disk array initialization complete..."
+echo "kamikazi-diskarrays: ${HBATYPE} disk array initialization complete..."
 
 # Keep supervisord from complaining this job is too short.
 sleep 3
