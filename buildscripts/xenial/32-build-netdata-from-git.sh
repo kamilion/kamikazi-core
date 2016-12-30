c51b67ddd81cb7d54fe13cdb6e384521997606bb
Graham Cantin <kamilion@gmail.com>
2016-12-29 23:52:11 -0800

    Unfuck netdata again, this time hopefully for permanant.

--- buildscripts/xenial/32-build-netdata-from-git.sh
+++ buildscripts/xenial/32-build-netdata-from-git.sh
@@ -14,10 +14,23 @@ cd /tmp/firehol
 git clone https://github.com/firehol/netdata --depth=1
 cd netdata
 sleep 2
+
 # Modify the installer to use addgroup and adduser instead.
-sed -i 's/groupadd -r/addgroup --system/' netdata-installer.sh
-sed -i 's/-d \/ netdata/--home \/ /' netdata-installer.sh
-sed -i 's/useradd -r -g netdata -c netdata -s /adduser --system --group netdata --gecos netdata --shell /' netdata-installer.sh
+#sed -i 's/groupadd -r/addgroup --system/' netdata-installer.sh
+#sed -i 's/-d \/ netdata/--home \/ /' netdata-installer.sh
+#sed -i 's/useradd -r -g netdata -c netdata -s /adduser --system --group netdata --gecos netdata --shell /' netdata-installer.sh
+
+# Well, netdata fucked it up as of https://github.com/firehol/netdata/commit/13ba361b51ccfd89587369181497776ea874a85f
+# On the up side, now they check to see if the user's already there first.
+# So we'll just take advantage of that.
+
+# Add the group for netdata.
+addgroup --system netdata
+# Add the user for netdata and set the group to netdata when it's created.
+adduser --system --group netdata --gecos netdata --shell /usr/sbin/nologin --home /
+
+# Now the installer should be happy with the existing user and skip trying to create it.
+
 # Run the installer.
 ./netdata-installer.sh --dont-wait --dont-start-it
 cp system/netdata.service /etc/systemd/system/netdata.service
