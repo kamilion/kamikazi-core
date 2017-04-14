70b32315fff18ad9752532ac237960374158216b
Graham Cantin <kamilion@gmail.com>
2017-04-14 02:12:34 -0700

    Switch to development branch for customizer

--- buildscripts/xenial/19-add-iso-customizer.sh
+++ buildscripts/xenial/19-add-iso-customizer.sh
@@ -8,6 +8,8 @@ cd /tmp/customizer-build
 
 git clone https://github.com/kamilion/customizer.git
 cd customizer
+# Switch to development branch
+git checkout development
 make PYQT=5 PYTHON=python3 && make install
 cd ..
 rm -Rf customizer
